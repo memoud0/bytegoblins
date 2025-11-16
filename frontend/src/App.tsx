efb6c63262fb5365c8ede676a37eafdb16b70b82
memoud0 <30944992+memoud0@users.noreply.github.com>
2025-11-16 11:11:42 -0500

    Update API endpoints to use production server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,24 @@ import LoginPage from "./pages/LoginPage.tsx";
 import ExplorePage from "./pages/ExplorePage.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
+import { useEffect } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  useEffect(() => {
+    async function pingBackend() {
+      try {
+        const res = await fetch(`${API_URL}/api/health`);
+        const data = await res.json();
+        console.log("Health check:", data);
+      } catch (err) {
+        console.error("Health check failed:", err);
+      }
+    }
+    pingBackend();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
