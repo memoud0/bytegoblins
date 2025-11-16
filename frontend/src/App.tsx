ed15c28c87673afc5a4fac8674e4edf1bd804078
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2025-11-15 23:15:29 -0500

    did explore page (menu missing)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={<ExplorePage />} />
+      <Route path="/explore" element={<ExplorePage />} />
     </Routes>
   );
 }
